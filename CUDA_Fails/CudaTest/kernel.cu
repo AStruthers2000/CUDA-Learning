b720a24f59f407951b6df439d47d29935ad2aaad
AStruthers2000 <struthers919@gmail.com>
2024-02-07 14:55:10 -0800

    added test project with working CS565 project 1 code

--- CUDA_Fails/CudaTest/kernel.cu
+++ CUDA_Fails/CudaTest/kernel.cu
@@ -6,24 +6,41 @@
 
 
 __global__ void addTwo(int* a, int* b, int* c){
-	int i = threadIdx.x;
+	//int i = threadIdx.x;
+	//int stride = blockDim.x;
+	//printf("I am threadIdx.x = %d and I have a stride of %d\n", i, stride);
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	printf("Thread %d on block %d has block dim %d and should update index %d\n", threadIdx.x, blockIdx.x, blockDim.x, i);
 	c[i] = a[i] + b[i];
 }
 
-int main() {
+int main(int argc, char** argv) {
 
 	srand(time(NULL));
-	const int arrLength = 1000;
+	int arrLength = 1000000;
+	if (argc == 2)
+	{
+		arrLength = atoi(argv[1]);
+	}
 
-	int a[arrLength];
-	int b[arrLength];
-	int c[arrLength];
+	int* a;
+	int* b;
+	int* c;
+
+	cudaMallocManaged(&a, arrLength * sizeof(int));
+	cudaMallocManaged(&b, arrLength * sizeof(int));
+	cudaMallocManaged(&c, arrLength * sizeof(int));
 
 	for (int i = 0; i < arrLength; i++) {
 		a[i] = rand() % 100;
 		b[i] = rand() % 100;
 	}
 
-	addTwo<<<1, 32>>>(a, b, c);
+	addTwo<<<2, 32>>>(a, b, c);
+	cudaDeviceSynchronize();
 
+	for (int i = 0; i < arrLength; i++)
+	{
+		printf("a[%d] = %d\tb[%d] = %d\tc[%d] = %d\n", i, a[i], i, b[i], i, c[i]);
+	}
 }
